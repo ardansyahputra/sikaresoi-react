9e3037092e3bb893a4f09e8f777490bb9f18d4f9
cartierssa <rizdian229@gmail.com>
2025-02-04 09:35:08 +0700

    fix

--- screen/auth/login/Login.js
+++ screen/auth/login/Login.js
@@ -8,21 +8,28 @@ import {
   Alert,
   Image,
   ImageBackground,
+  Dimensions,
 } from 'react-native';
 import * as Keychain from 'react-native-keychain';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import {useAuth} from '../AuthContext';
 import axios from 'axios';
 import useApiClient from '../../../src/api/apiClient';
 import {BarIndicator} from 'react-native-indicators';
 
+const {height} = Dimensions.get('window');
+
 const LoginScreen = ({navigation}) => {
   const [nip, setNip] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState({nip: '', password: ''});
   const {login, token, refreshToken} = useAuth();
   const apiClient = useApiClient();
   const [isLoading, setIsLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const [isMounted, setIsMounted] = useState(true); // Menambahkan state isMounted
+  const isValid = nip.length >= 8 && password.length >= 8;
 
   useEffect(() => {
     setIsMounted(true); // Menandai komponen sebagai terpasang
@@ -92,12 +99,10 @@ const LoginScreen = ({navigation}) => {
   }, [token, refreshToken, isMounted]);
 
   const loginHandler = async () => {
-    if (!nip || !password) {
-      console.error('Please fill in all fields.');
-      return;
-    }
+    if (!isValid) return;
 
     setIsLoading(true);
+    setError({nip: '', password: ''});
 
     try {
       const response = await apiClient.post('/auth/login', {nip, password});
@@ -114,13 +119,22 @@ const LoginScreen = ({navigation}) => {
           login(userData, token);
           navigation.replace('AppTabs');
         } else {
-          console.error('User data not found.');
+          setError({
+            nip: 'Invalid NIP or password',
+            password: 'Invalid NIP or password',
+          });
         }
       } else {
-        console.error('Token not found in response headers.');
+        setError({
+          nip: 'Invalid NIP or password',
+          password: 'Invalid NIP or password',
+        });
       }
     } catch (error) {
-      console.error('Login error:', error);
+      setError({
+        nip: 'Invalid NIP or password',
+        password: 'Invalid NIP or password',
+      });
     } finally {
       setIsLoading(false);
     }
@@ -139,20 +153,31 @@ const LoginScreen = ({navigation}) => {
     }
   };
 
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <View style={styles.container}>
       <ImageBackground
         source={require('../../admin/assets/images/poltekpol-barombong-bg.jpg')}
         style={styles.background}
-      />
+        resizeMode="cover">
+        <View style={styles.overlay} />
+      </ImageBackground>
+
       <View style={styles.formContainer}>
         <Image
           source={require('../../admin/assets/images/logo-default.png')}
           style={styles.logo}
         />
-        <Text style={styles.title}>Masuk</Text>
-        <View style={styles.inputContainer}>
-          <Icon name="user" size={20} color="#7f8c8d" style={styles.icon} />
+        <View style={[styles.inputContainer, error.nip && styles.inputError]}>
+          <Ionicons
+            name={'person'}
+            color="#7f8c8d"
+            size={15}
+            style={styles.icon}
+          />
           <TextInput
             style={styles.input}
             placeholder="NIP"
@@ -160,28 +185,51 @@ const LoginScreen = ({navigation}) => {
             value={nip}
             onChangeText={setNip}
           />
+          {error.nip && <Text style={styles.errorText}>{error.nip}</Text>}
         </View>
-        <View style={styles.inputContainer}>
-          <Icon name="lock" size={20} color="#7f8c8d" style={styles.icon} />
+        <View
+          style={[styles.inputContainer, error.password && styles.inputError]}>
+          <Ionicons
+            name={'lock-closed'}
+            color="#7f8c8d"
+            size={15}
+            style={styles.icon}
+          />
           <TextInput
             style={styles.input}
             placeholder="Password"
             placeholderTextColor="#BCC7CA"
             value={password}
             onChangeText={setPassword}
-            secureTextEntry
+            secureTextEntry={!showPassword}
           />
+          {error.password && (
+            <Text style={styles.errorText}>{error.password}</Text>
+          )}
+          <TouchableOpacity
+            onPress={togglePasswordVisibility}
+            style={styles.eyeIcon}>
+            <Ionicons
+              name={showPassword ? 'eye-outline' : 'eye-off-outline'}
+              size={18}
+              color="#7f8c8d"
+            />
+          </TouchableOpacity>
         </View>
         <TouchableOpacity
-          style={[styles.button, isLoading && styles.buttonDisabled]}
+          style={[styles.button, !isValid && styles.buttonDisabled]}
           onPress={loginHandler}
-          disabled={isLoading}>
+          disabled={!isValid || isLoading}>
           <Text style={styles.buttonText}>Masuk</Text>
         </TouchableOpacity>
       </View>
+
       {isLoading && (
         <View style={styles.loadingOverlay}>
-          <BarIndicator color="white" size={24} count={5} />
+          <View style={styles.loadingBox}>
+            <BarIndicator color="black" size={24} count={5} />
+            <Text style={styles.loadingText}>Memuat...</Text>
+          </View>
         </View>
       )}
     </View>
@@ -191,40 +239,36 @@ const LoginScreen = ({navigation}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-end',
+    position: 'relative',
   },
   background: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
     width: '100%',
-    height: '70%',
+    height: height * 0.8,
+    position: 'absolute',
+    resizeMode: 'cover',
+  },
+  overlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.2)', // Efek overlay jika ingin ditambahkan
   },
   formContainer: {
+    position: 'absolute',
+    bottom: 0,
     width: '100%',
     backgroundColor: '#fff',
-    borderTopLeftRadius: 16,
-    borderTopRightRadius: 16,
-    paddingVertical: 10,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingVertical: 30,
     paddingHorizontal: 30,
     alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {width: 0, height: -4},
-    shadowOpacity: 0.2,
-    shadowRadius: 5,
-    elevation: 6,
+    gap: 10,
+    zIndex: 5,
   },
   logo: {
-    width: 200,
-    height: 100,
+    width: 180,
+    height: 90,
     resizeMode: 'contain',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: '700',
-    marginBottom: 20,
-    color: '#34495e',
-    textAlign: 'center',
+    marginBottom: 10,
   },
   inputContainer: {
     flexDirection: 'row',
@@ -233,11 +277,18 @@ const styles = StyleSheet.create({
     height: 48,
     backgroundColor: '#f2f3f5',
     borderRadius: 10,
-    marginBottom: 20,
-    fontSize: 16,
-    color: '#34495e',
+    marginBottom: 12,
     borderWidth: 1,
     borderColor: '#dcdcdc',
+  },
+  inputError: {
+    borderColor: 'red',
+  },
+  errorText: {
+    color: 'red',
+    fontSize: 12,
+    fontFamily: 'Poppins-Regular',
+    marginRight: 10,
   },
   icon: {
     marginLeft: 15,
@@ -246,8 +297,9 @@ const styles = StyleSheet.create({
     flex: 1,
     height: 48,
     paddingHorizontal: 15,
-    fontSize: 16,
+    fontSize: 12,
     color: '#34495e',
+    fontFamily: 'Poppins-Regular',
   },
   button: {
     width: '100%',
@@ -256,15 +308,16 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 20,
+    marginTop: 10,
+    marginBottom: 10,
   },
   buttonText: {
     color: '#fff',
     fontSize: 16,
-    fontWeight: '600',
+    fontFamily: 'Poppins-SemiBold',
   },
   buttonDisabled: {
-    backgroundColor: '#95a5a6',
+    opacity: 0.5,
   },
   loadingOverlay: {
     position: 'absolute',
@@ -272,9 +325,36 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    backgroundColor: 'rgba(54, 54, 54, 0.5)',
     justifyContent: 'center',
     alignItems: 'center',
+    zIndex: 10,
+  },
+  loadingBox: {
+    width: '70%',
+    maxWidth: 200,
+    height: 120,
+    backgroundColor: 'white',
+    padding: 16,
+    borderRadius: 12,
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+    elevation: 6,
+  },
+  loadingText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'black',
+    marginTop: 12,
+  },
+
+  eyeIcon: {
+    paddingRight: 10,
   },
 });
 
